8b975a95c4acc9a35699055e105fcfb71525416c
ChocolateBread0 <37840725+ChocolateBread0@users.noreply.github.com>
2026-04-09 22:09:49 +0200

    Update Lights.js
    modify comments

--- Christmas-tree/script/Lights.js
+++ Christmas-tree/script/Lights.js
@@ -1,16 +1,12 @@
 // Lights.js
 const delay = ms => new Promise(res => setTimeout(res, ms));
 
-// Rimosso "export"
 async function showLight(ID) 
 {
     //-------------shuffle-------------
-    //prendo un array con tutti gli ID, lo mescolo e prendo solo il 30%
-    const totaleStelle = ID; // Il numero totale di "*"
+    const totaleStelle = ID; 
     const quantitaDaPrendere = Math.trunc(totaleStelle * 0.3);
 
-
-    //-------------shuffle VERO-------------
     let tuttiGliIndici = Array.from({ length: totaleStelle }, (_, i) => i);
 
     for (let i = tuttiGliIndici.length - 1; i > 0; i--) {
@@ -19,21 +15,21 @@ async function showLight(ID)
     }
 
     const casualiScelti = tuttiGliIndici.slice(0, Math.trunc(totaleStelle * 0.15));
-    //-------------shuffle VERO-------------
+    //-------------shuffle-------------
 
 
     while(true) {
         console.log("Eseguo operazione... " + new Date().toLocaleTimeString());
 
         for (let index = 0; index < casualiScelti.length; index++) {
-            // Alterniamo tra Ciano e Magenta in modo casuale
+            // We alternate randomly between Cyan and Magenta
             const isMagenta = Math.random() > 0.5;
             
             if (isMagenta) {
                 // Magenta Neon
                 document.getElementById(casualiScelti[index]).style.color = getColor(200, 255, 0, 50, 150, 255);
             } else {
-                // Ciano Neon
+                // Cyan Neon
                 document.getElementById(casualiScelti[index]).style.color = getColor(0, 50, 200, 255, 200, 255);
             }
         }
